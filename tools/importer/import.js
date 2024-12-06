26744dc6e907d8df21bcf351e13aed29ead24858
BasavarajKusugall <basavaraj.kusugall@gmail.com>
2024-12-06 14:47:34 +0530

    Added custom import rules

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -1,6 +1,53 @@
 const createCardBlock = (main, document) => {
-  const articles = document.querySelectorAll('.frame .design-style');
+  document.querySelectorAll('.design-style').forEach((tab, i) => {
+  const img = tab.querySelector('.design-style-image img')
+  let desc;
+  tab.querySelectorAll('.design-style-description').forEach((itm, i) => {
+    console.log(itm)
+    desc = itm;
+    if(!desc){
+      desc.append(itm);
+    }
 
+
+  })
+
+  const cells = [
+    ['Card'],
+    [img,desc],
+  ];
+  const table = WebImporter.DOMUtils.createTable(cells, document);
+  main.prepend(table);
+  })
+};
+const createMetadataBlock = (main, document) => {
+  const meta = {};
+
+  // find the <title> element
+  const title = document.querySelector('title');
+  if (title) {
+    meta.Title = title.innerHTML.replace(/[\n\t]/gm, '');
+  }
+
+  // find the <meta property="og:description"> element
+  const desc = document.querySelector('[name="description"]');
+  if (desc) {
+    meta.Description = desc.content;
+  }
+
+   const navigationtitle = document.querySelector('[name="navigationtitle"]');
+    if (navigationtitle) {
+      meta.Navigationtitle = navigationtitle.content;
+    }
+
+  // find the <meta property="og:image"> element
+  const img = document.querySelector('[property="og:image"]');
+  if (img) {
+    // create an <img> element
+    const el = document.createElement('img');
+    el.src = img.content;
+    meta.Image = el;
+  }
 
   // helper to create the metadata block
   const block = WebImporter.Blocks.getMetadataBlock(document, meta);
@@ -11,25 +58,63 @@ const createCardBlock = (main, document) => {
   // returning the meta object might be usefull to other rules
   return meta;
 };
-
 export default {
-  transformDOM: ({ document }) => {
-    const main = document.querySelector('body');
+  /**
+   * Apply DOM operations to the provided document and return
+   * the root element to be then transformed to Markdown.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @returns {HTMLElement} The root element to be transformed
+   */
+  transformDOM: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    // define the main element: the one that will be transformed to Markdown
+    const main = document.body;
 
-
-createMetadataBlock(main,document);
-    // final cleanup
+    // attempt to remove non-content elements
     WebImporter.DOMUtils.remove(main, [
-            'header',
-            'nav',
-            '.nav',
-            'footer',
-            '.footer',
-            'iframe',
-            'noscript','.disclaimer',
+      'header',
+      '.header',
+      'nav',
+      '.nav',
+      'footer',
+      '.footer',
+      'iframe',
+      'noscript',
     ]);
-    WebImporter.Blocks.convertBlocksToTables(main.querySelector('article'),document)
+    createMetadataBlock(main, document);
+    WebImporter.rules.transformBackgroundImages(main, document);
+    WebImporter.rules.adjustImageUrls(main, url, params.originalURL);
+    WebImporter.rules.convertIcons(main, document);
+    createCardBlock(main, document);
 
     return main;
+  },
+
+  /**
+   * Return a path that describes the document being transformed (file name, nesting...).
+   * The path is then used to create the corresponding Word document.
+   * @param {HTMLDocument} document The document
+   * @param {string} url The url of the page imported
+   * @param {string} html The raw html (the document is cleaned up during preprocessing)
+   * @param {object} params Object containing some parameters given by the import process.
+   * @return {string} The path
+   */
+  generateDocumentPath: ({
+    // eslint-disable-next-line no-unused-vars
+    document, url, html, params,
+  }) => {
+    let p = new URL(url).pathname;
+    if (p.endsWith('/')) {
+      p = `${p}index`;
+    }
+    return decodeURIComponent(p)
+      .toLowerCase()
+      .replace(/\.html$/, '')
+      .replace(/[^a-z0-9/]/gm, '-');
   },
 };
